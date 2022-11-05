14415cbd4abcc252e74185e02a30a05a693d4ed0
Aleksandr Vasiljev <zaswet@gmail.com>
2022-11-05 16:45:14 +0200

    image adding , and routing fix

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -5,6 +5,20 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './components/App';
 
+//SweetAlert2
+import Swal from 'sweetalert2/dist/sweetalert2.js';
+import 'sweetalert2/dist/sweetalert2.css';
+window.Swal = Swal;
+const toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+
+});
+window.toast = toast;
+
 ReactDom.createRoot(document.getElementById('app')).render(
     <BrowserRouter>
         <App/>
--- resources/js/components/product/New.jsx
+++ resources/js/components/product/New.jsx
@@ -7,7 +7,28 @@ const New = () => {
      const [image, setImage] = useState('');
      const [type, setType] = useState('');
      const [price, setPrice] = useState('');
-     const [inventory, setInventory] = useState('');
+     const [quantity, setQuantity] = useState('');
+
+     const changeHandler = (e) => {
+
+        let file = e.target.files[0]
+        let reader = new FileReader()
+        let limit = 2024 * 2024 * 4
+        if(file['size'] > limit) {
+            Swal.fire({
+                type: 'error',
+                title: 'Ooops....',
+                text: 'Something Went Wrong',
+                footer: 'Why do i have this issue?',
+
+            })
+        }
+        reader.onloadend = (file) => {
+            console.log(reader.result);
+            setImage(reader.result)
+        }
+        reader.readAsDataURL(file)
+     }
 
     return(
 
@@ -26,22 +47,22 @@ const New = () => {
                         <div className="wrapper-left">
                             <div className="card">
                                 <p>Name</p>
-                                <input type="text" name="name" />
+                                <input type="text" name="name" value={name} onChange={(e)=>{setName(e.target.value)}} />
 
                                 <p>Description</p>
-                                <textarea name="description" cols="10" rows ="5"></textarea>
+                                <textarea name="description" cols="10" rows ="5" value={description} onChange={(e)=>{setDescription(e.target.value)}}></textarea>
 
                                 <div className="media">
                                     <div className="images-list">
                                         <li className="image-item">
                                             <div className="image-item-img">
-                                                <img src="" alt="" width="117px" height="100px" />
+                                                <img src={image} alt="upload-image" width="117px" height="100px" />
                                             </div>
                                         </li>
                                         <li className="image-item">
                                             <form className="image-item-form">
                                                 <label className="image-item-form--label">Add Image</label>
-                                                <input type="file" name="image" className="image-item-form--input" />
+                                                <input type="file" name="image" className="image-item-form--input" onChange={changeHandler}/>
                                             </form>
                                         </li>
                                     </div>
@@ -51,17 +72,17 @@ const New = () => {
                         <div className="wrapper-right">
                             <div className="card">
                                 <p>Product Type</p>
-                                <input type="text" name="type" />
+                                <input type="text" name="type" value={type} onChange={(e)=>{setType(e.target.value)}} />
 
                                 <hr className="hr" />
                                 
-                                <p>Inventory</p>
-                                <input type="text" name="inventory" />
+                                <p>Quantity</p>
+                                <input type="number" name="quantity" value={quantity} onChange={(e)=>{setQuantity(e.target.value)}} />
 
                                 <hr className="hr" />
 
                                 <p>Price</p>
-                                <input type="number" name="price" />
+                                <input type="number" name="price" value={price} onChange={(e)=>{setPrice(e.target.value)}} />
 
                                 <br className="br" />
                             </div>
